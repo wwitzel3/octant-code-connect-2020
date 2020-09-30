27d4fbf4eae2dcfcb2369cab3d8918b216c53ad5
Wayne Witzel III <wayne@riotousliving.com>
2020-09-29 23:07:25 -0400

    fetch table information from api service
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

--- plugin/src/content.ts
+++ plugin/src/content.ts
@@ -7,33 +7,14 @@ import { CardFactory } from "@project-octant/plugin/components/card";
 import { FlexLayoutFactory } from "@project-octant/plugin/components/flexlayout";
 
 const PREFIX = "/code-connect-2020";
-const ENTITY_COLUMNS = ["ID", "Name", "URL", "Tags"];
-const ENTITIES = [
-  {
-    id: 1,
-    name: "EntityA",
-    url: "https://example.com/EA",
-    tags: ["a1", "a2"],
-    detail: "Details about EntityA",
-  },
-  {
-    id: 2,
-    name: "EntityB",
-    url: "https://example.com/EB",
-    tags: ["b1", "b2"],
-    detail: "Details about EntityB",
-  },
-];
+const DEPLOYMENT_COLUMNS = ["Name", "Title", "Filename", "Deployment"];
 
 /**
  * getHandler returns the details of a specific entity
  * @param this instance of octant.Plugin
  * @param params router params and properties of octant.ContentRequest
  */
-export function getHandler(
-  this: any,
-  params: any
-): octant.ContentResponse {
+export function getHandler(this: any, params: any): octant.ContentResponse {
   const name: string = params.entityName;
   const title = [
     new TextFactory({ value: "Code Connect 2020" }),
@@ -56,7 +37,9 @@ export function getHandler(
         "Information from the Kubernetes API using dashboardClient for " + name,
     }).toComponent(),
     factoryMetadata: {
-      title: [new TextFactory({ value: ` ${name} - Details II` }).toComponent()],
+      title: [
+        new TextFactory({ value: ` ${name} - Details II` }).toComponent(),
+      ],
     },
   }).toComponent();
 
@@ -80,36 +63,29 @@ export function getHandler(
  * @param this instance of octant.Plugin
  * @param params router params and properties of octant.ContentRequest
  */
-export function listHandler(
-  this: any,
-  params: any
-): octant.ContentResponse {
+export function listHandler(this: any, params: any): octant.ContentResponse {
   const title = [
     new TextFactory({ value: "Code Connect 2020" }),
-    new TextFactory({ value: "Entities" }),
+    new TextFactory({ value: "Deployments" }),
   ];
 
-  const table = new h.TableFactoryBuilder(title, ENTITY_COLUMNS);
-  ENTITIES.forEach((entity) => {
-    const row = {
-      ID: new TextFactory({ value: entity.id.toString() }),
-      Name: new LinkFactory({
-        value: entity.name,
-        ref: `${PREFIX}/entities/${entity.name}`,
-      }),
-      URL: new LinkFactory({
-        value: entity.url,
-        ref: entity.url,
-      }),
-      // Use TextFactory with isMarkdown true to render custom elements.
-      Tags: new TextFactory({
-        value: entity.tags
-          .map((t) => `<span class="badge badge-purple">${t}</span>`)
-          .join(""),
-        options: { isMarkdown: true },
-      }),
-    } as h.TableRow;
-    table.push(row);
+  const table = new h.TableFactoryBuilder(title, DEPLOYMENT_COLUMNS);
+
+  const httpClient = this.httpClient as octant.HTTPClient;
+
+  httpClient.getJSON("http://localhost:4200/deployments", (results: any) => {
+    results.forEach((deployment: any) => {
+      const row = {
+        Name: new LinkFactory({
+          value: deployment.name,
+          ref: `${PREFIX}/deployments/${deployment.name}`,
+        }),
+        Title: new TextFactory({ value: deployment.title }),
+        Filename: new TextFactory({ value: deployment.filename }),
+        Deployment: new TextFactory({ value: "Not installed" }),
+      } as h.TableRow;
+      table.push(row);
+    });
   });
 
   return h.createContentResponse(title, [table.getFactory()]);
@@ -120,10 +96,7 @@ export function listHandler(
  * @param this instance of octant.Plugin
  * @param params router params and properties of octant.ContentRequest
  */
-export function notFoundHandler(
-  this: any,
-  param: any
-): octant.ContentResponse {
+export function notFoundHandler(this: any, param: any): octant.ContentResponse {
   const title = [
     new TextFactory({ value: "Code Connect 2020" }),
     new TextFactory({ value: "Not found" }),
